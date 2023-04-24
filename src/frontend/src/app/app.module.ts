1d8f29ba0db306e31abd39723656e60d48e043c5
KW <k.wojtasinski@wp.pl>
2023-04-24 18:29:06 +0200

    [Frontend] First components
    Added example componentes for Category and Product
    Modified name app

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './components/category/categories/categories.component';
+import { AddCategoryComponent } from './components/category/add-category/add-category.component';
+import { ViewCategoryComponent } from './components/category/view-category/view-category.component';
+import { ProductsComponent } from './components/product/products/products.component';
+import { AddProductsComponent } from './components/product/add-products/add-products.component';
+import { ViewProductsComponent } from './components/product/view-products/view-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    AddCategoryComponent,
+    ViewCategoryComponent,
+    ProductsComponent,
+    AddProductsComponent,
+    ViewProductsComponent
   ],
   imports: [
     BrowserModule,
