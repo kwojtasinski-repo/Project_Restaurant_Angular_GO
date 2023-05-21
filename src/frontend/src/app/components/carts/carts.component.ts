a16cd7e3dbb2cac6e0fba905aab9fdc9082001b4
KW <k.wojtasinski@wp.pl>
2023-05-21 16:13:29 +0200

    [Frontend] Carts Improvements

--- src/frontend/src/app/components/carts/carts.component.ts
+++ src/frontend/src/app/components/carts/carts.component.ts
@@ -1,29 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { take } from 'rxjs';
+import { Subscription, take } from 'rxjs';
 import { Cart } from 'src/app/models/cart';
-import { CartService } from 'src/app/services/cart.service';
+import { Product } from 'src/app/models/product';
+import { fetchCart, finalizeCart, removeProductFromCart } from 'src/app/stores/cart/cart.actions';
+import { getCart } from 'src/app/stores/cart/cart.selectors';
+import { CartState } from 'src/app/stores/cart/cart.state';
 
 @Component({
   selector: 'app-carts',
   templateUrl: './carts.component.html',
   styleUrls: ['./carts.component.scss']
 })
-export class CartsComponent implements OnInit {
+export class CartsComponent implements OnInit, OnDestroy {
   public cart: Cart = { products: [] };
   public isLoading: boolean = true;
+  private getCart$: Subscription = new Subscription();
+  public cart$ = this.cartStore.select(getCart);
 
-  constructor(private cartService: CartService, private spinnerService: NgxSpinnerService) { }
-
+  constructor(private spinnerService: NgxSpinnerService, private cartStore: Store<CartState>) { }
+  
   public ngOnInit(): void {
     this.spinnerService.show();
-    this.cartService.getCart()
-      .pipe(take(1))
+    this.cartStore.dispatch(fetchCart());
+    this.getCart$ = this.cartStore.select(getCart)
       .subscribe(c => {
-        setTimeout(() => {
         this.cart = c;
         this.isLoading = false;
-        this.spinnerService.hide();});
+        this.spinnerService.hide();
       });
+  }
+
+  public ngOnDestroy(): void {
+    this.getCart$.unsubscribe();
+  }
+
+  public deleteProduct(product: Product): void {
+    this.cartStore.dispatch(removeProductFromCart({ product }));
+    this.cartStore.dispatch(fetchCart());
+  }
+
+  public calculateTotal(): number {
+    return this.cart.products.reduce((total, product) => total + product.price, 0);
+  }
+
+  public finalizeOrder(): void {
+    // need redirect to url with order
+    this.cartStore.dispatch(finalizeCart());
   }
 }
--- src/frontend/src/app/services/cart.service.ts
+++ src/frontend/src/app/services/cart.service.ts
@@ -14,12 +14,12 @@ export class CartService {
   constructor() { }
 
   public add(product: Product): Observable<void> {
-    this.cart.products.push(product);
+    this.cart = { products: [...this.cart.products, product] };
     return new Observable((ob) => { ob.next(); ob.complete(); });
   }
 
   public delete(product: Product): Observable<void> {
-    this.cart.products = this.cart.products.filter(p => p.id !== product.id);
+    this.cart = { products: this.cart.products.filter(p => p.id !== product.id) };
     return new Observable((ob) => { ob.next(); ob.complete(); });
   }
 
