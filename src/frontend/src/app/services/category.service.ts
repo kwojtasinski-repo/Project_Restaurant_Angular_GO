cd60f1b8667d8910a27399c33a38cefd4c9a83d0
KW <k.wojtasinski@wp.pl>
2023-06-03 00:17:06 +0200

    [Frontend] Category
    Integrate with backend

--- src/frontend/src/app/services/category.service.ts
+++ src/frontend/src/app/services/category.service.ts
@@ -1,52 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Category } from '../models/category';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+  private backendUrl = (window as any )['__env']['backendUrl'];
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   public add(category: Category): Observable<void> {
-    this.categories.push(category);
-    return new Observable((ob) => { ob.next(); ob.complete(); });
+    return this.httpClient.post<void>(`${this.backendUrl}/api/categories`, category, { withCredentials: true });
   }
 
   public update(category: Category): Observable<void> {
-    const response: Observable<void> = new Observable((ob) => { ob.next(); ob.complete(); });
-    const index = this.categories.findIndex(c => c.id === category.id);
-    if (index < 0) {
-      return response;
-    }
-    this.categories[index] = category;
-    return response;
+    return this.httpClient.put<void>(`${this.backendUrl}/api/categories/${category.id}`, category, { withCredentials: true });
   }
 
   public getAll(): Observable<Category[]> {
-    return of(this.categories);
+    return this.httpClient.get<Category[]>(`${this.backendUrl}/api/categories`, { withCredentials: true });
   }
 
   public get(id: number): Observable<Category | undefined> {
-    return of(this.categories.find(c => c.id === id));
+    return this.httpClient.get<Category | undefined>(`${this.backendUrl}/api/categories/${id}`, { withCredentials: true });
   }
-
-  private categories: Category[] = [
-    {
-      id: 1,
-      name: 'Category #1',
-      deleted: false
-    },
-    {
-      id: 2,
-      name: 'Category #2',
-      deleted: false
-    },
-    {
-      id: 3,
-      name: 'Category #3',
-      deleted: true
-    },
-  ];
 }
