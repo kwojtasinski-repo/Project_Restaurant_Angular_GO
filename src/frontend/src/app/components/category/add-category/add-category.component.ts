8d27554084c0c4b824b1430f7db34fb79d038d73
KW <k.wojtasinski@wp.pl>
2024-07-07 16:50:08 +0200

    [Frontend] Improvement

--- src/frontend/src/app/components/category/add-category/add-category.component.ts
+++ src/frontend/src/app/components/category/add-category/add-category.component.ts
@@ -3,7 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Category } from 'src/app/models/category';
 import { CategoryState } from 'src/app/stores/category/category.state';
-import { Subject, debounceTime, takeUntil } from 'rxjs';
+import { Subject, debounceTime, map, takeUntil } from 'rxjs';
 import { getError } from 'src/app/stores/category/category.selectors';
 import * as CategoryActions from 'src/app/stores/category/category.actions';
 import { getValidationMessage } from 'src/app/validations/validations';
@@ -16,7 +16,6 @@ import { getValidationMessage } from 'src/app/validations/validations';
 export class AddCategoryComponent implements OnInit, OnDestroy {
   public error$ = this.store.select(getError);
   public categoryForm: FormGroup = new FormGroup({});
-  public category: Category | undefined;
   private categoryFormValueChanged$ = new Subject();
 
   constructor(private store: Store<CategoryState>) { }
@@ -28,15 +27,17 @@ export class AddCategoryComponent implements OnInit, OnDestroy {
   }
 
   public ngAfterViewInit(): void {
-    this.categoryForm.valueChanges.pipe(debounceTime(10), takeUntil(this.categoryFormValueChanged$))
-      .subscribe((value) => this.store.dispatch(CategoryActions.categoryFormUpdate({
+    this.categoryForm.valueChanges.pipe(
+      debounceTime(10),
+      takeUntil(this.categoryFormValueChanged$),
+      map((value) => this.store.dispatch(CategoryActions.categoryFormUpdate({
         category: {
           id: '',
           name: value.categoryName,
           deleted: false
         }
-      })
-    ));
+      })))
+    ).subscribe();
   }
 
   public ngOnDestroy(): void {
