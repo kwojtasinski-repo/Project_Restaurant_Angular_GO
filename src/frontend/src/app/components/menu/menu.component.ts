dc2374212dd4302a6432ab98a908198e01bca063
KW <k.wojtasinski@wp.pl>
2024-11-23 14:19:20 +0100

    [Frontend] Improved searching with signals

--- src/frontend/src/app/components/menu/menu.component.ts
+++ src/frontend/src/app/components/menu/menu.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, WritableSignal, computed, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
-import { BehaviorSubject, EMPTY, Observable, catchError, finalize, map, shareReplay, take, tap } from 'rxjs';
+import { EMPTY, catchError, finalize, shareReplay, tap } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { CartState } from 'src/app/stores/cart/cart.state';
 import { addProductToCart, clearErrors } from 'src/app/stores/cart/cart.actions';
@@ -28,42 +29,38 @@ export class MenuComponent implements OnInit, OnDestroy {
   private spinnerService = inject(NgxSpinnerService);
 
   public user$ = this.loginStore.select(LoginSelectors.getUser);
-  public products$: Observable<Product[]> = new BehaviorSubject([]);
-  public productsToShow$: Observable<Product[]> = new BehaviorSubject([]);
-  public term: string = '';
-  public error: string | undefined;
-  public cartError$ = this.cartStore.select(getError);
+  public products: WritableSignal<Product[]> = signal([]);
+  public term: WritableSignal<string> = signal('');
+  public error: WritableSignal<string | undefined> = signal(undefined);
+  public cartError = toSignal(this.cartStore.select(getError));
   
+  public productsToShow = computed(() => {
+    const term = this.term();
+    return this.products().filter(p =>
+      p.name.toLocaleLowerCase().startsWith(term.toLocaleLowerCase())
+    );
+  });
+
   public ngOnInit(): void {
-    this.products$ = this.productService.getAll()
+    this.productService.getAll()
       .pipe(
-        take(1),
         tap(() => this.spinnerService.show()),
         shareReplay(),
         finalize(() => this.spinnerService.hide()),
         catchError((error) => {
           if (error.status === 0) {
-            this.error = 'Sprawdź połączenie z internetem';
+            this.error.set('Sprawdź połączenie z internetem');
           } else if (error.status === 500) {
-            this.error = 'Coś poszło nie tak, spróbuj ponownie później';
+            this.error.set('Coś poszło nie tak, spróbuj ponownie później');
           }
           console.error(error);
           return EMPTY;
         })
-      );
-
-    this.productsToShow$ = this.products$;
-    this.products$.subscribe();
+      ).subscribe(products => this.products.set(products));
   }
 
   public ngOnDestroy(): void {
     this.cartStore.dispatch(clearErrors());
-  }
-
-  public search(term: string): void {
-    this.productsToShow$ = this.products$.pipe(
-      map(products => products.filter(p => p.name.toLocaleLowerCase().startsWith(term.toLocaleLowerCase())))
-    );
   }
 
   public addToCart(product: Product): void {
