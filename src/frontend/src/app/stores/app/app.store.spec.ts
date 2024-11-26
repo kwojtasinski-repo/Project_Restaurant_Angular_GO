fb9b0612dd6b958517908cc50f4ad5efbf0bc6c9
KW <k.wojtasinski@wp.pl>
2024-11-26 23:16:16 +0100

    [Frontend] App Store Unit Tests

--- src/frontend/src/app/stores/app/app.store.spec.ts
+++ src/frontend/src/app/stores/app/app.store.spec.ts
@@ -13,6 +13,65 @@ describe('AppStore', () => {
   })
 
   it('should be created', () => {
+    // Arrange Act Assert
     expect(appStore).toBeTruthy();
+  });
+
+  it('should have the initial showHeader value as false', () => {
+    // Arrange Act Assert
+    expect(appStore.showHeader()).toBeFalse();
+  });
+
+  it('should have the initial currentUrl value as an empty string', () => {
+    // Arrange Act Assert
+    expect(appStore.currentUrl()).toBe('');
+  });
+
+  describe('enableHeader()', () => {
+    it('should set showHeader to true', () => {
+      // Arrange Act
+      appStore.enableHeader();
+
+      // Assert
+      expect(appStore.showHeader()).toBeTrue();
+    });
+  });
+
+  describe('disableHeader()', () => {
+    it('should set showHeader to false', () => {
+      // Arrange
+      appStore.enableHeader();
+
+      //  Act
+      appStore.disableHeader();
+
+      // Assert
+      expect(appStore.showHeader()).toBeFalse();
+    });
+  });
+
+  describe('setCurrentUrl()', () => {
+    it('should update currentUrl to the provided value', () => {
+      // Arrange Act Assert
+      const newUrl = '/new-url';
+
+      // Act
+      appStore.setCurrentUrl(newUrl);
+
+      // Assert
+      expect(appStore.currentUrl()).toBe(newUrl);
+    });
+
+    it('should update currentUrl to an empty string when called with an empty string', () => {
+      // Arrange
+      appStore.setCurrentUrl('newUrl');
+      const newUrl = '';
+
+      // Act
+      appStore.setCurrentUrl(newUrl);
+
+      // Assert
+      expect(appStore.currentUrl()).toBe(newUrl);
+    });
   });
 });
